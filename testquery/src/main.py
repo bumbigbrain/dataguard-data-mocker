4c1323c1db78ef83dfadc983bbdbf4c06ffd21fe
Owen Suphanat <owen0813715430@hotmail.com>
2024-05-11 21:27:45 +0700

    updated testquery

--- testquery/src/main.py
+++ testquery/src/main.py
@@ -17,12 +17,39 @@ def main():
 
     cursor = conn.cursor()
 
+    print("ALL RECORD DESC ORDERD QUERY")
+    all_record_desc_ordered_query = "SELECT * FROM V$DATAGUARD_STATS ORDER BY CAST(TIME_COMPUTED AS DATETIME) DESC;"
+    cursor.execute(all_record_desc_ordered_query)
+    for item in cursor:
+        print(item)
 
+
+
+    print("")    
+    print("LATEST APPLY FINISH TIME QUERY")
+    apply_finish_time_query = "SELECT * FROM V$DATAGUARD_STATS WHERE NAME='APPLY_FINISH_TIME' ORDER BY CAST(TIME_COMPUTED AS DATETIME) DESC LIMIT 1;" 
+    cursor.execute(apply_finish_time_query)
+    for item in cursor:
+        print(item)
+
+
+
+    print("") 
+    print("LATEST APPLY LAG QUERY")
+    latest_apply_lag_query = "SELECT * FROM V$DATAGUARD_STATS WHERE NAME='APPLY_LAG' ORDER BY CAST(TIME_COMPUTED AS DATETIME) DESC LIMIT 1;" 
+    cursor.execute(latest_apply_lag_query)
+    for item in cursor:
+        print(item)
+
+
+    print("")    
+    print("LATEST TRANSPORT LAG QUERY")
     latest_transport_lag_query = "SELECT * FROM V$DATAGUARD_STATS WHERE NAME='TRANSPORT_LAG' ORDER BY CAST(TIME_COMPUTED AS DATETIME) DESC LIMIT 1;" 
     cursor.execute(latest_transport_lag_query)
     for item in cursor:
-        for att in item:
-            print(att)
+        print(item)
+
+
 
 if __name__ == "__main__":
     main()
