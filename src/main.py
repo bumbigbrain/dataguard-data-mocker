ea15e2ee9d308c5a60f3bb0a5c485529a5241456
Owen Suphanat <owen0813715430@hotmail.com>
2024-05-11 20:41:18 +0700

    refactor to object

--- src/main.py
+++ src/main.py
@@ -6,78 +6,95 @@ import random
 
 
 
-def create_DATAGUARD_STATS_TABLE():
-    create_dataguard_stats_table = """
-    CREATE TABLE V$DATAGUARD_STATS (
-        SOURCE_DBID INTEGER,
-        SOURCE_DB_UNIQUE_NAME VARCHAR (32),
-        NAME VARCHAR (32),
-        VALUE VARCHAR (64),
-        UNIT VARCHAR (30),
-        TIME_COMPUTED VARCHAR (30),
-        DATUM_TIME VARCHAR (30),
-        CON_ID INTEGER
-    ) """
-    return create_dataguard_stats_table
+class Mocker():
+    def __init__(self, user, host, port, database, password):
+        self.conn = Mocker.initConnection(user, host, port, database, password)
+        self.cursor = self.conn.cursor()
 
-
-
-def randomTimeInterval():
-    random_minute = random.randint(53, 59)
-    random_second = random.randint(10, 50)
-    
-    return f"+00 00:{random_minute}:{random_second}"
+    def initConnection(user, host, port, database, password):
+        conn = mysql.connector.connect(
+            user=user,
+            host=host,
+            password=password,
+            port=port,
+            database=database
+        )
+        return conn
+         
     
 
+    def create_DATAGUARD_STATS_TABLE(self):
+        create_dataguard_stats_table = """
+        CREATE TABLE V$DATAGUARD_STATS (
+            SOURCE_DBID INTEGER,
+            SOURCE_DB_UNIQUE_NAME VARCHAR (32),
+            NAME VARCHAR (32),
+            VALUE VARCHAR (64),
+            UNIT VARCHAR (30),
+            TIME_COMPUTED VARCHAR (30),
+            DATUM_TIME VARCHAR (30),
+            CON_ID INTEGER
+        ) """
+        self.cursor.execute(create_dataguard_stats_table)
 
-def Mocker(cursor, conn):
 
 
-    for i in range(10):    
-        print("start generating") 
-        head = "INSERT INTO V$DATAGUARD_STATS VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"    
-        # random theseq 
-        APPLY_FINISH_TIME_VALUE = randomTimeInterval()  
-        APPLY_LAG = randomTimeInterval() 
-        TRANSPORT_LAG =  randomTimeInterval()
+    def randomTimeInterval():
+        random_minute = random.randint(53, 59)
+        random_second = random.randint(10, 50)
+        
+        return f"+00 00:{random_minute}:{random_second}"
+        
 
 
-        val = [
-            (112, "DB01", "APPLY_FINISH_TIME", f"{APPLY_FINISH_TIME_VALUE}", "interval", f"{str(datetime.now())}", f"{str(datetime.now())}", 1),
-            (112, "DB01", "APPLY_LAG", f"{APPLY_LAG}", "interval", f"{str(datetime.now())}", f"{str(datetime.now())}", 1),
-            (112, "DB01", "TRANSPORT_LAG", f"{TRANSPORT_LAG}", "interval", f"{str(datetime.now())}", f"{str(datetime.now())}", 1)
-        ]
-            
-        cursor.executemany(head, val)
-        conn.commit()
-        print("waiting for 1s")
-        time.sleep(3)
+    def generateVDATAGUARD_STATS(self):
+
+
+        for i in range(10):    
+            print("start generating") 
+            head = "INSERT INTO V$DATAGUARD_STATS VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"    
+            # random theseq 
+            APPLY_FINISH_TIME_VALUE = Mocker.randomTimeInterval()  
+            APPLY_LAG = Mocker.randomTimeInterval() 
+            TRANSPORT_LAG =  Mocker.randomTimeInterval()
+
+
+            val = [
+                (112, "DB01", "APPLY_FINISH_TIME", f"{APPLY_FINISH_TIME_VALUE}", "interval", f"{str(datetime.now())}", f"{str(datetime.now())}", 1),
+                (112, "DB01", "APPLY_LAG", f"{APPLY_LAG}", "interval", f"{str(datetime.now())}", f"{str(datetime.now())}", 1),
+                (112, "DB01", "TRANSPORT_LAG", f"{TRANSPORT_LAG}", "interval", f"{str(datetime.now())}", f"{str(datetime.now())}", 1)
+            ]
+                
+            self.cursor.executemany(head, val)
+            self.conn.commit()
+            print("waiting for 3s")
+            time.sleep(3)
+
+
+
+
+
 
 
 
 
 
 def main():
+
     try:
-        conn = mysql.connector.connect(
+        mocker = Mocker(
             user="root",
-            password="1q2w3e4r",
             host="127.0.0.1",
+            password="1q2w3e4r",
             port=3306,
             database="dataguard"
+
         )
-
-    except mysql.connector.Error as e:
-        print(f"{e}")
-    
-
-    cur = conn.cursor()
-    # nowTime = str(datetime.now())
-    # print(nowTime, type(nowTime))
+    except:
+        print("error")
     
     
-    Mocker(cur, conn)
-    
+    mocker.generateVDATAGUARD_STATS() 
     
     
     
